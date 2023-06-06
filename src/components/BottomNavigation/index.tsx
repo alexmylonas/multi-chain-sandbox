f7e9e732e552242eb141a3742e920dd013add671
Alex Mylonas <alex.a.mylonas@gmail.com>
2023-06-06 14:02:05 +0300

    fix: corrected border width

--- src/components/BottomNavigation/index.tsx
+++ src/components/BottomNavigation/index.tsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css, keyframes } from 'styled-components';
 import { DARK_GRAY, PURPLE } from '../../constants';
 import IconHome from '../../icons/IconHome';
 import IconGrid from '../../icons/IconGrid';
@@ -6,17 +6,21 @@ import { Routes } from '../ConnectedRoutes';
 import React from 'react';
 import IconSwap from '../../icons/IconSwap';
 
+const NavPositioner = styled.div`
+  position: sticky;
+  width: 100%;
+  bottom: 0;
+`;
+
 const NavWrapper = styled.div`
   background-color: ${DARK_GRAY};
-  position: sticky;
-  bottom: 0;
+  position: relative;
   display: flex;
   justify-content: space-around;
   width: 100%;
 `;
 
-const NavItem = styled.button<{ active?: boolean }>`
-  (props:{active?:boolean})display: flex;
+const NavItem = styled.button<{ active: boolean }>`
   align-items: center;
   border: none;
   background: none;
@@ -27,8 +31,18 @@ const NavItem = styled.button<{ active?: boolean }>`
   // also top border should be purple
   width: 100%;
   padding: 16px 0;
-  // background-color: ${(props) => (props.active ? 'red' : 'transparent')};
-  border-top: ${(props) => (props.active ? `8px solid ${PURPLE}` : '8px solid transparent')};
+  // border-top: 2px solid ${({ active }) => (active ? PURPLE : 'transparent')};
+`;
+
+const Border = styled.div<{ activeIndex: number; width: number }>`
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 2px;
+  width: ${({ width }) => width}px;
+  background-color: ${PURPLE};
+  transition: transform 0.3s ease-in-out;
+  transform: translateX(${({ activeIndex }) => activeIndex * 100}%);
 `;
 
 type Props = {
@@ -37,18 +51,40 @@ type Props = {
 };
 
 const BottomNavigation = React.memo(({ navigate, active }: Props) => {
+  const tabWrapperRef = React.useRef<HTMLDivElement>(null);
+  const [tabWidth, setTabWidth] = React.useState(0);
+
+  React.useEffect(() => {
+    if (!tabWrapperRef.current) {
+      return;
+    }
+    const handleResize = () => {
+      const componentWidth = tabWrapperRef.current.offsetWidth / 3;
+      setTabWidth(componentWidth);
+    };
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
-    <NavWrapper>
-      <NavItem active={active === Routes.Home} onClick={() => navigate(Routes.Home)}>
-        <IconHome height="24px" width="24px" />
-      </NavItem>
-      <NavItem active={active === Routes.NFTs} onClick={() => navigate(Routes.NFTs)}>
-        <IconGrid height="24px" width="24px" />
-      </NavItem>
-      <NavItem active={active === Routes.Trading} onClick={() => navigate(Routes.Trading)}>
-        <IconSwap height="24px" width="24px" />
-      </NavItem>
-    </NavWrapper>
+    <NavPositioner>
+      <NavWrapper ref={tabWrapperRef}>
+        <NavItem active={active === Routes.Home} tabIndex={Routes.Home} onClick={() => navigate(Routes.Home)}>
+          <IconHome height="24px" width="24px" />
+        </NavItem>
+        <NavItem active={active === Routes.NFTs} tabIndex={Routes.NFTs} onClick={() => navigate(Routes.NFTs)}>
+          <div>
+            <IconGrid height="24px" width="24px" />
+          </div>
+        </NavItem>
+        <NavItem active={active === Routes.Trading} tabIndex={Routes.Trading} onClick={() => navigate(Routes.Trading)}>
+          <IconSwap height="24px" width="24px" />
+        </NavItem>
+        <Border activeIndex={active} width={tabWidth} />
+      </NavWrapper>
+    </NavPositioner>
   );
 });
 
