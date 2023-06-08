8d49fd7a77f72050fbe922e1a29cc32c3cc8a48e
Alex Mylonas <alex.a.mylonas@gmail.com>
2023-06-08 20:13:14 +0300

    feat: added useListHistory hook for fetching NFTs

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,6 +5,7 @@ export const API_URL = process.env.REACT_APP_BASE_API_URL;
 
 export enum Endpoints {
   Collectibles = '/collectibles/v1',
+  History = '/history/v2',
 }
 
 export const apiUrl = (path:Endpoints) => `${API_URL}${path}`;
---
+++ src/services/useListHistory.ts
@@ -0,0 +1,29 @@
+import { useQuery } from '@tanstack/react-query';
+import { Endpoints, apiUrl } from './api';
+import { ChainIds, ListHistoryInput, ListHistoryOutput } from './types';
+
+const useListHistory = (address: string) => {
+  const input: ListHistoryInput = {
+    accounts: [
+      {
+        address,
+        chainId: ChainIds.Ethereum,
+      },
+    ],
+  };
+  const { isLoading, error, data } = useQuery<ListHistoryOutput>({
+    queryKey: ['transactions', address],
+    queryFn: () =>
+      fetch(apiUrl(Endpoints.History), {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(input),
+      }).then((res) => res.json()),
+  });
+
+  return { isLoading, error, data: data };
+};
+
+export default useListHistory;
