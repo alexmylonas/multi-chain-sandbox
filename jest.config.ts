c3518364abff34d63b40ebb14ae5b387b3897f0d
Alex Mylonas <alex.a.mylonas@gmail.com>
2023-06-09 03:28:12 +0300

    chore: added babel

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,31 @@
 // The jest configuartion
 // https://jestjs.io/docs/en/configuration.html
-module.exports = {
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  // Coverage configuration
-  collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.{js,jsx}'],
-  coverageDirectory: 'coverage',
+  rootDir: '.',
+  testMatch: ['**/__tests__/**/*.test.tsx'],
+  transform: {
+    '\\.(tj)sx$': 'babel-jest',
+  },
+  setupFilesAfterEnv: ['./src/__tests__/setup.ts'], 
 
+  moduleNameMapper: {
+    '^components(.*)$': '<rootDir>/src/components$1',
+    '^hooks(.*)$': '<rootDir>/src/hooks$1',
+    '^services(.*)$': '<rootDir>/src/services$1',
+    '^icons(.*)$': '<rootDir>/src/icons$1',
+    '^utils(.*)$': '<rootDir>/src/utils$1',
+    '^types(.*)$': '<rootDir>/src/types$1',
+    '^pages(.*)$': '<rootDir>/src/pages$1',
+    '^__tests__(.*)$': '<rootDir>/src/__tests__$1',
+    "uuid": require.resolve('uuid')
+  },
+
+  collectCoverageFrom: ['src/**/*.{ts,tsx}'],
+  coverageDirectory: 'coverage'
 };
+
+export default config;
